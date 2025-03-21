5c98d097b2f63336032c66e33b906acd97aba81a
Dushyant98474839 <sdushyant074@gmail.com>
2025-03-22 03:35:38 +0530

    Fixed npm dependency issues and updated package.json

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { skills } from "@/constants/skills";
-import { Tilt } from "react-tilt";
+
 import { IconCloudDemo } from "../sub/IconCloud";
 import { motion } from "framer-motion";
 import {
